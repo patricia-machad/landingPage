3e80e6c56a30b255e5b47ee73bd857220c7316fe
patricia-machad <pathsilva09@gmail.com>
2021-01-15 22:52:01 -0300

    Projeto: Landing Page, tema livre

---
+++ js/script.js
@@ -0,0 +1,79 @@
+
+/*
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementsByName(
+por Classe: getElementsByClassName()
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+assunto.style.width = '100%'
+
+function validaNome() {
+    let txt = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txt.innerHTML = 'Nome inválido'
+        txt.style.color = 'red'   
+    } else {
+        txt.innerHTML = 'Nome válido'
+        txt.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Excedeu o limite do texto, digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function send(){
+    if (nomeOk == true && emailOk == true && assuntoOk == true){
+        alert('Formulado enviado com sucesso!')
+    } else{
+        alert('Preencha o formulario corretamente antes de enviar')
+    }
+}
+
+function mapaZoom(){
+    mapa.style.width = '500px'
+    mapa.style.height = '350px'
+}
+
+function mapaNormal(){
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+    
+}
